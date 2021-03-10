e03cf077020dfdbd9e6686da39fccb7c9570e447
Vivek <vivek.15@mountblue.tech>
2021-03-10 14:35:32 +0530

    added .env file define port in it, created logic for validate status code and delay in second

--- server.js
+++ server.js
@@ -1,67 +1,72 @@
 const express = require("express");
-const { v1 : uuid }=require("uuid");
+const dotenv = require("dotenv");
+const { v4: uuidV4 } = require("uuid");
+const path = require("path");
 
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.get("/html", (req, res) => {
-	res.set("Content-Type", "text/html");
-	res.send(`<!DOCTYPE html>
-                <html>
-                <head>
-                </head>
-                <body>
-                    <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
-                    <p> - Martin Fowler</p>
-                </body>
-                </html>`);
-	res.end();
+	res.status(200).sendFile(path.join(__dirname, "index.html"), (err) => {
+		if (err) {
+			res.sendStatus(500);
+			console.error(err);
+		}
+		res.end();
+	});
 });
 
 app.get("/json", (req, res) => {
-	res.json({
-		slideshow: {
-			author: "Yours Truly",
-			date: "date of publication",
-			slides: [
-				{
-					title: "Wake up to WonderWidgets!",
-					type: "all",
-				},
-				{
-					items: [
-						"Why <em>WonderWidgets</em> are great",
-						"Who <em>buys</em> WonderWidgets",
-					],
-					title: "Overview",
-					type: "all",
-				},
-			],
-			title: "Sample Slide Show",
-		},
+	res.status(200).sendFile(path.join(__dirname, "data.json"), (err) => {
+		if (err) {
+			res.sendStatus(500);
+			console.err(err);
+		}
+		res.end();
 	});
-    res.end();
 });
 
 app.get("/uuid", (req, res) => {
-    res.send({
-        uuid :uuid()
-    });
-    res.end();
+	res
+		.status(200)
+		.json({
+			uuid: uuidV4(),
+		})
+		.end();
 });
 
-app.get("/status/:code",(req,res)=>{
-    const code = Number(req.params.code);
-    res.status(code);
-    res.send(`${code} status code`)
-    res.end();
+app.get("/status/:statusCode", (req, res) => {
+	const code = Number(req.params.statusCode);
+	try {
+		res.sendStatus(code).end();
+	} catch (err) {
+		res
+			.status(400)
+			.json({
+				message: `Invalid status code : ${req.params.statusCode} `,
+			})
+			.end();
+	}
 });
 
-app.get('/delay/:id',(req,res)=>{
-    let id = Number(req.params.id);
-    setTimeout(function() {
-        res.send(`delay ${id} seconds`);
-    }, id*1000);
+app.get("/delay/:delayInSecond", (req, res) => {
+	const id = parseInt(req.params.delayInSecond);
+
+	if (Number.isInteger(id) && id >= 0) {
+		setTimeout(function () {
+			res.json({
+				'message':`delay ${id} seconds`
+			}).end();
+		}, id * 1000);
+	} else {
+		res
+			.status(400)
+			.json({
+				message: "Invalid delay",
+			})
+			.end();
+	}
 });
 
 app.listen(PORT, () => console.log(`Run on port : ${PORT}`));
