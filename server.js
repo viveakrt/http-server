74a47d8c24610056e8cb4a075ad4abdadcff1255
Vivek <vivek.15@mountblue.tech>
2021-03-10 10:36:36 +0530

    Added in server.js /uuid and /status/{status_code} return a response with status code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { v1 : uuid }=require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,27 +20,40 @@ app.get("/html", (req, res) => {
 
 app.get("/json", (req, res) => {
 	res.json({
-        "slideshow": {
-            "author": "Yours Truly",
-            "date": "date of publication",
-            "slides": [
-                {
-                "title": "Wake up to WonderWidgets!",
-                "type": "all"
-                },
-                {
-                "items": [
-                    "Why <em>WonderWidgets</em> are great",
-                    "Who <em>buys</em> WonderWidgets"
-                ],
-                "title": "Overview",
-                "type": "all"
-                }
-            ],
-            "title": "Sample Slide Show"
-            }
-        });
-    
+		slideshow: {
+			author: "Yours Truly",
+			date: "date of publication",
+			slides: [
+				{
+					title: "Wake up to WonderWidgets!",
+					type: "all",
+				},
+				{
+					items: [
+						"Why <em>WonderWidgets</em> are great",
+						"Who <em>buys</em> WonderWidgets",
+					],
+					title: "Overview",
+					type: "all",
+				},
+			],
+			title: "Sample Slide Show",
+		},
+	});
+    res.end();
+});
+
+app.get("/uuid", (req, res) => {
+    res.json({
+        uuid :uuid()
+    });
+    res.end();
+});
+
+app.get("/status/:code",(req,res)=>{
+    let code = req.params.code;
+    res.status(code);
+    res.end();
 });
 
 
