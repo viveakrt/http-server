85ce7a8fa48e129b8969dc0a93ce74d16dc0c5a5
Vivek <vivek.15@mountblue.tech>
2021-03-10 10:12:59 +0530

    get /json added in server.js to return the json string

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const PORT =process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/html',(req,res)=>{
-    res.set('Content-Type', 'text/html');
-    res.send(`<!DOCTYPE html>
-    <html>
-      <head>
-      </head>
-      <body>
-          <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
-          <p> - Martin Fowler</p>
+app.get("/html", (req, res) => {
+	res.set("Content-Type", "text/html");
+	res.send(`<!DOCTYPE html>
+                <html>
+                <head>
+                </head>
+                <body>
+                    <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
+                    <p> - Martin Fowler</p>
+                </body>
+                </html>`);
+	res.end();
+});
+
+app.get("/json", (req, res) => {
+	res.json({
+        "slideshow": {
+            "author": "Yours Truly",
+            "date": "date of publication",
+            "slides": [
+                {
+                "title": "Wake up to WonderWidgets!",
+                "type": "all"
+                },
+                {
+                "items": [
+                    "Why <em>WonderWidgets</em> are great",
+                    "Who <em>buys</em> WonderWidgets"
+                ],
+                "title": "Overview",
+                "type": "all"
+                }
+            ],
+            "title": "Sample Slide Show"
+            }
+        });
     
-      </body>
-    </html>`);
-    res.end();
 });
 
 
-
-app.listen(PORT,()=>console.log(`Run on port : ${PORT}`));
+app.listen(PORT, () => console.log(`Run on port : ${PORT}`));
